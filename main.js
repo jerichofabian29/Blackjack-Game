260aee3635da33b8a77339e9bde1072211f84a62
Jericho Fabian <jerichofabian8@gmail.com>
2024-07-28 22:27:09 +0800

    added dynamic buttons

--- main.js
+++ main.js
@@ -3,17 +3,22 @@ let secondCard = 1;
 let total = firstCard + secondCard;
 let cards = [firstCard, secondCard]; // storage for cards
 
-let hasBlackJack = false;
-let isAlive = true;
-
 let message = ""
 let messageEL = document.querySelector("#message-el");
 let totalEl = document.querySelector("#total-el");
 let cardsEl = document.querySelector("#cards-el");
 let startGame = document.querySelector("#startGame");
 
+let newCardbtn = document.querySelector("#newCardbtn");
+    newCardbtn.style.display = "none";
+let startGamebtn = document.querySelector("#startGamebtn");
+    startGamebtn.style.display = "block";
+let restartGamebtn = document.querySelector("#restartGamebtn");
+    restartGamebtn.style.display = "none";
+
 startGame = () => {
     renderGame();
+    newCardbtn.style.display = "block";
 }
 
 renderGame = () => {
@@ -24,19 +29,28 @@ renderGame = () => {
 
     if(total <= 20){
         message = "Do you want to draw a new card?";
+        restartGamebtn.style.display = "block";
     } 
     else if(total === 21){
         message = "You've got Blackjack!";
-        hasBlackJack = true;
+        newCardbtn.style.display = "none";
+        startGamebtn.style.display = "none";
+        restartGamebtn.style.display = "block";
+
+
     }
     else {
         message = "You're out of the game!";
-        isAlive = false;
+        newCardbtn.style.display = "none";
+        startGamebtn.style.display = "none";
+        
     }
     messageEL.textContent = message;
 }
 
 newCard = () => {
+    restartGamebtn.style.display = "block";
+
     let card = Math.floor(Math.random() * 12); // random num between 0 and 11
 
 
@@ -54,6 +68,11 @@ resetGame = () => {
     total = firstCard + secondCard;
     totalEl.textContent = "Total: ";
     cardsEl.textContent = "Cards: ";
+
+    startGamebtn.style.display = "block";
+    newCardbtn.style.display = "none";
+    restartGamebtn.style.display = "none";
+
 }
 
 
