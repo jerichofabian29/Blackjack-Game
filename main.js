5514afa659d662ded8e92b82e21d51a706b3a69c
Jericho Fabian <jerichofabian8@gmail.com>
2024-07-28 20:32:55 +0800

    linked the text contents to JS

--- main.js
+++ main.js
@@ -6,21 +6,30 @@ let hasBlackJack = false;
 let isAlive = true;
 
 let message = ""
+let messageEL = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
-startGame = () =>{
+startGame = () => {
     if(sum <= 20){
-        message = "Do you want to draw a new card? 😊";
+        message = "Do you want to draw a new card?";
     } 
     else if(sum === 21){
-        message = "Wohoo! You've got Blackjack! 🤑";
+        message = "You've got Blackjack!";
         hasBlackJack = true;
     }
     else {
-        message = "You're out of the game! 😝";
+        message = "You're out of the game!";
         isAlive = false;
     }
-    console.log(message);
+    messageEL.textContent = message;
+    sumEl.textContent += sum;
+    cardsEl.textContent += firstCard + ", " + secondCard;
 }
+
+
+
+
 
 
 
