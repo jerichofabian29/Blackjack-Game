3bed3f7cf41ae976b429f01754305b30868c6adb
Jericho Fabian <jerichofabian8@gmail.com>
2024-07-28 21:34:47 +0800

    completed all functionalities

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = 1;
+let secondCard = 1;
 let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard]; // storage for cards
 
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,8 +10,17 @@ let message = ""
 let messageEL = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
+let startGame = document.querySelector("#startGame");
 
 startGame = () => {
+    renderGame();
+}
+
+renderGame = () => {
+    //render out firstCard and secondCard
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: "   + " " + cards;
+
     if(sum <= 20){
         message = "Do you want to draw a new card?";
     } 
@@ -23,10 +33,27 @@ startGame = () => {
         isAlive = false;
     }
     messageEL.textContent = message;
-    sumEl.textContent += sum;
-    cardsEl.textContent += firstCard + ", " + secondCard;
 }
 
+newCard = () => {
+    console.log("Drawing a new card from the deck!");
+    let card = Math.floor(Math.random() * 12); // random num between 0 and 11
+
+
+    sum += card
+    cards.push(card);
+    renderGame();
+}
+
+resetGame = () => {
+    message = "Want to play a round?";
+    messageEL.textContent = message;
+
+
+    cards = [];
+    sumEl.textContent = "Sum: ";
+    cardsEl.textContent = "Cards: "   + " " + cards;
+}
 
 
 
