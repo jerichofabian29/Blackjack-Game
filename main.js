e69ebd091658876c4f3541ed9800258672819132
Jericho Fabian <jerichofabian8@gmail.com>
2024-07-28 20:08:39 +0800

    styled the UI and added some elements

---
+++ main.js
@@ -0,0 +1,26 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = ""
+
+startGame = () =>{
+    if(sum <= 20){
+        message = "Do you want to draw a new card? 😊";
+    } 
+    else if(sum === 21){
+        message = "Wohoo! You've got Blackjack! 🤑";
+        hasBlackJack = true;
+    }
+    else {
+        message = "You're out of the game! 😝";
+        isAlive = false;
+    }
+    console.log(message);
+}
+
+
+
